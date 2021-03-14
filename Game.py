411338a984dedda6b166bd6c1d435acbd95d2e4c
SriwanKm <kcqkoo@gmail.com>
2021-03-14 16:52:53 -0700

    clean up code

--- Game.py
+++ Game.py
@@ -1,3 +1,8 @@
+import random
+weapons = ['Rock', 'Paper', 'Scissors']
+# print(weapons[random.randint(0, 2)])
+
+
 print('Welcome to Rock-Paper-Scissors Game!')
 start = input('Type r, p, or s to fight with Rock, Paper, or Scissors: ')
 
@@ -9,20 +14,44 @@ player_history = {
     "Scissors": 0
 }
 
+opponent_output = {
+    "current": ""
+}
 
-def opponent_output():
-    print(max(player_history, key=player_history.get))
+
+def opp_output():
+    max_weapon = max(player_history, key=player_history.get)
+    if player_history.get('Rock') == player_history.get('Paper') == player_history.get('Scissors'):
+        opponent_output['current'] = weapons[random.randint(0, 2)]
+    elif max_weapon == 'Rock':
+        opponent_output['current'] = 'Paper'
+        # print('The opponent fight back with', 'PAPER!')
+    elif max_weapon == 'Paper':
+        opponent_output['current'] = 'Scissors'
+        # print('The opponent fight back with', 'SCISSORS!')
+    elif max_weapon == 'Scissors':
+        opponent_output['current'] = 'Rock'
+        # print('The opponent fight back with', 'ROCK!')
+    return opponent_output['current']
+
+
+def print_opp_output():
+    print('The opponent fight back with', opp_output())
+
 
 
 def add_to_history(user_input):
+    print_opp_output()
     if user_input == 'r':
         player_history['Rock'] += 1
+        print('Your weapon is ROCK')
     elif user_input == 'p':
         player_history['Paper'] += 1
+        print('Your weapon is PAPER')
     else:
         player_history['Scissors'] += 1
+        print('Your weapon is SCISSORS')
     print(player_history)
-    opponent_output()
     reprint = input('Type r, p, or s to fight with Rock, Paper, or Scissors: ')
     validate_input(reprint)
 
